05c02673567bbb6ee553442ea86449c9ee7a0f4e
Ansh kumar <anshg3800@gmail.com>
2026-07-09 00:12:55 +0530

    Time: 37 ms (23.33%), Space: 123.2 MB (95%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,91 +1,45 @@
-import java.util.*;
-
 class Solution {
-    static final int MOD = 1_000_000_007;
+    private static final int MOD = 1000000007;
+    private static final int MAX = 100001;
+    private static final int[] pow = new int[MAX];
+
+    static {
+        pow[0] = 1;
+        for (int i = 1; i < MAX; i++)
+            pow[i] = (int) ((pow[i - 1] * 10L) % MOD);
+    }
 
     public int[] sumAndMultiply(String s, int[][] queries) {
         int n = s.length();
-
-        // Store positions and values of non-zero digits
-        ArrayList<Integer> posList = new ArrayList<>();
-        ArrayList<Integer> digitList = new ArrayList<>();
+        int[] A = new int[n + 1];
+        int[] B = new int[n + 1];
+        int[] len = new int[n + 1];
 
         for (int i = 0; i < n; i++) {
-            int d = s.charAt(i) - '0';
-            if (d != 0) {
-                posList.add(i);
-                digitList.add(d);
+            int d = s.charAt(i) - '0';            
+            A[i + 1] = A[i] + d;
+            
+            if (d > 0) {
+                B[i + 1] = (int) ((B[i] * 10L + d) % MOD);
+                len[i + 1] = len[i] + 1;
+            } else {
+                B[i + 1] = B[i];
+                len[i + 1] = len[i];
             }
         }
 
-        int m = posList.size();
+        int[] res = new int[queries.length];
+        int i = 0;
 
-        int[] pos = new int[m];
-        int[] prefixSum = new int[m + 1];
-        long[] prefixNum = new long[m + 1];
-        long[] pow10 = new long[m + 1];
-
-        pow10[0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            pos[i] = posList.get(i);
-            int d = digitList.get(i);
-
-            prefixSum[i + 1] = prefixSum[i] + d;
-            prefixNum[i + 1] = (prefixNum[i] * 10 + d) % MOD;
-            pow10[i + 1] = (pow10[i] * 10) % MOD;
+        for (int[] q : queries) {
+            int l = q[0], r = q[1] + 1;
+            
+            long sub = ((long) B[l] * pow[len[r] - len[l]]) % MOD;
+            long x = (B[r] - sub + MOD) % MOD;
+            
+            res[i++] = (int) ((x * (A[r] - A[l])) % MOD);
         }
 
-        int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-
-            int left = lowerBound(pos, l);
-            int right = upperBound(pos, r) - 1;
-
-            if (left > right) {
-                ans[i] = 0;
-                continue;
-            }
-
-            int len = right - left + 1;
-
-            long x = (prefixNum[right + 1]
-                    - prefixNum[left] * pow10[len] % MOD + MOD) % MOD;
-
-            long sum = prefixSum[right + 1] - prefixSum[left];
-
-            ans[i] = (int) (x * sum % MOD);
-        }
-
-        return ans;
-    }
-
-    // First index with value >= target
-    private int lowerBound(int[] arr, int target) {
-        int l = 0, r = arr.length;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] < target)
-                l = mid + 1;
-            else
-                r = mid;
-        }
-        return l;
-    }
-
-    // First index with value > target
-    private int upperBound(int[] arr, int target) {
-        int l = 0, r = arr.length;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] <= target)
-                l = mid + 1;
-            else
-                r = mid;
-        }
-        return l;
+        return res;
     }
 }
